5aaa0ce6cac20db895586c6fc194caab0ff44f9d
Ameet Jayawant <ameetj85@gmail.com>
2023-10-20 09:16:09 -0400

    Commit & Sync from Home

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
-const socket = require("socket.io");
+const socket = require('socket.io');
 var cors = require('cors');
 
 const app = express();
@@ -9,38 +9,40 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http, {
   cors: {
     origin: '*',
-    methods: ['GET', 'POST']
-  }
+    methods: ['GET', 'POST'],
+  },
 });
 
-const corsOptions = { origin: "*:5500" };
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+};
 
 app.use(cors(corsOptions));
 
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
 
-io.on("connection", function (socket) {
-  console.log("Client made socket connection");
-  setTimeout(function(){
+io.on('connection', function (socket) {
+  console.log('Client made socket connection');
+  setTimeout(function () {
     socket.send('Sent a message 4seconds after connection!');
- }, 4000);
+  }, 4000);
 
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
-    console.log('A user disconnected'); 
- });
+    console.log('A user disconnected');
+  });
 
-  socket.on('message', function(data) {
+  socket.on('message', function (data) {
     console.log('Received message from client:', data);
-  })
+  });
 
   // listen for incoming data msg on this newly connected socket
-  socket.on('data',function (data) {
-    console.log(`data received is '${data}'`)
+  socket.on('data', function (data) {
+    console.log(`data received is '${data}'`);
   });
 });
-
 
 // Test GET
 app.get('/', (req, res) => {
@@ -50,7 +52,6 @@ app.get('/', (req, res) => {
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice', (request, response) => {
-
   // Get information about the incoming call
   console.log(request.body);
   const city = request.body.FromCity;
@@ -59,20 +60,17 @@ app.post('/voice', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
   try {
-    io.emit('incomingCall', {data: phoneNumber});  
+    io.emit('incomingCall', { data: phoneNumber });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-  
+
   twiml.say(
     { voice: 'Polly.Amy', loop: 1 },
     `You are calling from ${phoneNumber}. `
   );
 
-  twiml.say(
-    { voice: 'Polly.Amy', loop: 1 },
-    'Please enjoy our music.'
-  );
+  twiml.say({ voice: 'Polly.Amy', loop: 1 }, 'Please enjoy our music.');
   twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
 
   // Render the response as XML in reply to the webhook request
@@ -85,7 +83,7 @@ app.post('/voice', (request, response) => {
 //   console.log('Now listening on port 5500. ');
 // });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
 
